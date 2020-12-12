faea614a01504bf4e83d04c94d29667293ec6841
faizahmaddae <faizahmad.daiee@gmail.com>
2020-12-12 13:46:32 +0430

    1

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -4,14 +4,13 @@ class CameraPage extends StatefulWidget {
   @override
   _CameraPageState createState() => _CameraPageState();
 }
-
 class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey.withOpacity(0.2),
       child: new Center(
-        child: new Text('Your Camera Appear here'),
+        child: new Text('Your Camera Appear here!'),
       ),
     );
   }
